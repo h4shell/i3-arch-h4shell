13473127fe4c620ceefe74dd51107edc1efdb737
h4shell <60841743+h4shell@users.noreply.github.com>
2021-06-27 14:57:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,13 @@ sudo pacman -Sy i3status-rust nitrogen ttf-font-awesome picom terminator
 cp -r $HOME/.config/i3 $HOME/.config/i3_backup
 cp -r $HOME/.config/terminator $HOME/.config/terminator_backup
 
+#wallpaper
+# mkdir $HOME/.wallpaper
+cp pak/.config/wallpaper1.png $HOME/.wallpaper/
 
 #install
 cp -r pak/.config/i3 $HOME/.config/
 cp -r pak/.config/i3status-rust $HOME/.config/
 cp -r pak.config/terminator $HOME/.config/
+
+nitrogen --set-auto $HOME/.wallpaper/wallpaper1.png --save
