93eb50810be453b84ec913f66d25bfdf4e85c847
h4shell <60841743+h4shell@users.noreply.github.com>
2021-06-27 13:07:07 +0200

    Commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!bash
+
+sudo pacman -Sy i3status-rust nitrogen ttf-font-awesome proton
+
+#backup
+cp -r $HOME/.config/i3 $HOME/.config/i3_backup
+
+
+#install
+
+cp -r pak/.config/i3 $HOME/.config/
+cp -r pak/.config/i3status-rust $HOME/.config/
