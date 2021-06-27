8d7024f09d7ea355be6a81c406133236a1d69be7
h4shell <60841743+h4shell@users.noreply.github.com>
2021-06-27 14:52:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo pacman -Sy i3status-rust nitrogen ttf-font-awesome picom
+sudo pacman -Sy i3status-rust nitrogen ttf-font-awesome picom terminator
 
 #backup
 cp -r $HOME/.config/i3 $HOME/.config/i3_backup
@@ -6,7 +6,6 @@ cp -r $HOME/.config/terminator $HOME/.config/terminator_backup
 
 
 #install
-
 cp -r pak/.config/i3 $HOME/.config/
 cp -r pak/.config/i3status-rust $HOME/.config/
 cp -r pak.config/terminator $HOME/.config/
