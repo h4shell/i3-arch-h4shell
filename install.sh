f31c39911d5cee217cdfff7ea74f291c10086c98
h4shell <60841743+h4shell@users.noreply.github.com>
2021-06-27 15:12:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ cp -r $HOME/.config/i3 $HOME/.config/i3_backup
 cp -r $HOME/.config/terminator $HOME/.config/terminator_backup
 
 #wallpaper
-# mkdir $HOME/.wallpaper
+mkdir $HOME/.wallpaper
 cp pak/.config/wallpaper1.png $HOME/.wallpaper/
 
 #install
 cp -r pak/.config/i3 $HOME/.config/
 cp -r pak/.config/i3status-rust $HOME/.config/
-cp -r pak.config/terminator $HOME/.config/
+cp -r pak/.config/terminator $HOME/.config/
 
 nitrogen --set-auto $HOME/.wallpaper/wallpaper1.png --save
